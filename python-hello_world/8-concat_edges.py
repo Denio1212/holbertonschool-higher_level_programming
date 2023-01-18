b847d811e4345621c6a7b757f4634f88e79b0271
Denio1212 <shehuden22@yahoo.com>
2023-01-18 03:21:55 -0800

    plez

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[38:64] + str[107:110] + str[0:5])
-print(str)
+print(str[39:67] + str[107:112] + str[0:6])
