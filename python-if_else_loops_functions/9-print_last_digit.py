bae60411dcd06ac47b9e1a9532e934dcdd9cf08e
Denio1212 <shehuden22@yahoo.com>
2023-01-19 01:59:36 -0800

    latest news

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number < 0:
+        lastd = -number % 10
+    elif number > 0:
+        lastd = number % 10
+    else:
+        lastd = 0
+    print("{:d}".format(lastd), end="")
+    return lastd
