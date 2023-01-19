5bb0e00f830f32f3f5acbf0c69c538d4b00d50d9
Denio1212 <shehuden22@yahoo.com>
2023-01-19 01:51:40 -0800

    pycodefix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-def islower(c):
+def lower(c):
     return ord(c) in range(ord('a'), ord('z') + 1)
+
 
 def uppercase(str):
     s = ''
     for i in str:
-        if islower(i):
-            s+= chr(ord(i) - ord(' '))
+        if lower(i):
+            s += chr(ord(i) - ord(' '))
         else:
             s += i
     print("{}".format(s))
