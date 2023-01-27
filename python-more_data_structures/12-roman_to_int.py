60e6bd8702abb4d68031e49b74fa86022a620672
Denio1212 <shehuden22@yahoo.com>
2023-01-27 11:45:02 -0800

    Donbass

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,14 @@
 def roman_to_int(roman_string):
     result = 0
     romannum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    large = 1
     if type(roman_string) == str:
-        for i in roman_string:
+        for i in roman_string[::-1]:
             if i in romannum:
-                result += romannum[i]
-                continue
+                if romannum[i] >= large:
+                    result += romannum[i]
+                    large = romannum[i]
+                else:
+                    result -= romannum[i]
 
     return result       
