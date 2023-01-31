09e347dcaed77d38aec5b61fafdafb4583b05755
Denio1212 <shehuden22@yahoo.com>
2023-01-31 10:52:58 -0800

    validated square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,10 +4,10 @@
 
 class Square:
     'Holds a size and validation for it'
-    def __init__(self, size):
+    def __init__(self, size=0):
         'initializer'
-        self.size = size
-        if size != int:
+        self.__size = size
+        if type(size) != int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
