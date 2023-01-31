608fd14ca7eb48b620434af30d436ae1b974762b
Denio1212 <shehuden22@yahoo.com>
2023-01-31 13:04:45 -0800

    Squared fr

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -12,6 +12,11 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        if type(position[0]) != int or type(position[1]):
+             raise TypeError('position must be a tuple of 2 positive integers')
+        if position[0] < 0 or position[1] < 0:
+             raise TypeError('position must be a tuple of 2 positive integers')
+
 
     def area(self):
         'Returns the current area of square'
