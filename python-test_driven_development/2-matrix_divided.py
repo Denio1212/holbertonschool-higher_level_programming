fd15a6d54f554358d08a6e26014cd7016bbca0e6
Denio1212 <shehuden22@yahoo.com>
2023-02-03 11:11:11 -0800

    Matrix conquered/

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,7 @@ def matrix_divided(matrix, div):
         for i in matrix:
             for j in i:
                 if type(j) is not float and type(j) is not int:
-                    raise TypeError("matrix must be a matrix (list of lists)of integers/floats")
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
                 if len(i) != len(matrix[0]):
                     raise TypeError("Each row of the matrix must have the same size")
                 if type(div) is not int and type(div) is not float:
