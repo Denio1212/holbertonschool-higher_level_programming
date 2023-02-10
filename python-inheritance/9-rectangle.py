c51ac3c6b7f9134164a9f97cae980b0eca634468
Denio1212 <shehuden22@yahoo.com>
2023-02-10 18:23:54 +0100

    No longer very WIP

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Finally a complete rectangle
+"""
+BaseGeometry = __import__("7-base_geometry.py").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A new derivative of BaseGeo
+    """
+
+    def __init__(self, width, height):
+        """
+        Famous constructor
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """
+        WIP
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        returns the object as a nice str
+        """
+        return "{{}/{}".format(self.__width, self.__height)
