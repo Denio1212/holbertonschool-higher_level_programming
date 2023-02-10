13ca5137aa3ab2d0901550d631762fbcea9595bf
Denio1212 <shehuden22@yahoo.com>
2023-02-10 15:27:30 +0100

    WIP

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Improvements have been made
+"""
+
+
+class BaseGeometry:
+    """
+    A class with an aream method
+    """
+    def area(self):
+        raise Exeption("area() is not implemented")
