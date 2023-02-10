9ce372c4e776d8b59696511baefac0665420310b
Denio1212 <shehuden22@yahoo.com>
2023-02-10 15:01:24 +0100

    Chacker

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Returns true if object is of same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Is instance will be used
+    """
+    if isinstance(obj, a_class) is True:
+        return True
+    else:
+        return False
