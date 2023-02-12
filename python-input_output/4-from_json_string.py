76b5d48149b48d336e1322287f805c0788f7cdce
Denio1212 <shehuden22@yahoo.com>
2023-02-12 19:22:28 +0100

    Prints out a json str as a python obj

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Returns an object represented by JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Read up m8
+    """
+    return json.load(my_str)
