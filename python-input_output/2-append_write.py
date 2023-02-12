a11058394ab9f469933b859b6dc369fb72dac5f3
Denio1212 <shehuden22@yahoo.com>
2023-02-12 19:07:14 +0100

    Adding at EOF

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -9,4 +9,4 @@ def append_write(filename="", text=""):
     The function to append at EOF
     """
     with open(filename, mode="w", encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
