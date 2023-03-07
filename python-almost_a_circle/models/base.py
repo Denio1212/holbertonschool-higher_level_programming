1ed4cda89bce8e6408e046c04cb26f6f82ec40e0
Denio1212 <shehuden22@yahoo.com>
2023-03-07 12:25:55 +0100

    jason loaded

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,3 +30,25 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the json to a file
+        """
+        a_list = []
+        file = cls.__name__ + '.json'
+        if list_objs is not None:
+            for idx in list_objs:
+                a_list.append(idx.to_dictionary())
+        with open(file, mode='w') as f:
+            f.write(cls.to_json_string(a_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        loads a list from json_strings
+        """
+        if json_string is not None or json_string != "":
+            return json.loads(json_string)
+
