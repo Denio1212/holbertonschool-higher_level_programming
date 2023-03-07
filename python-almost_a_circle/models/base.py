9aae0df8a6f03913825314db9bb419a24872f616
Denio1212 <shehuden22@yahoo.com>
2023-03-07 12:14:38 +0100

    jason

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,6 +21,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         returns JSON string of a list of dicts
@@ -29,3 +30,4 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
