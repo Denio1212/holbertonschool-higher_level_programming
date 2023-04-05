4c76474e52b7f40c20132afa167850617e338666
Denio <shehuden22@yahoo.com>
2023-04-05 16:14:00 +0200

    Table with cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Added cities table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CRATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256),
+    FOREIGN KEY (state_id) REFERENCES (states_id)
+);
