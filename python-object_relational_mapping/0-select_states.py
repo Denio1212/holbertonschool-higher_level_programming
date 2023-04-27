95d282fa73ab2cc175960c27f9426a2b9b44e4f0
Denio1212 <shehuden22@yahoo.com>
2023-04-27 04:23:53 -0700

    fixed typo

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Selects all states from the USA database
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MYSQLdb
+
+    with MYSQLdb.connect(host="localhost", user=argv[1], password=argv[2], database=argv[3], port="3306") as db:
+        db.execute("SELECT * FROM states ORDER BY id ASC")
+        table = db.fetchall()
+        for data in table:
+            print(data)
