6886998d8e9eff823023b5241ebf30644404aed8
Denio <shehuden22@yahoo.com>
2023-04-27 14:19:35 +0200

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+So we filter now. makes a script that lists all states with an
+upper N
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    with MySQLdb.connect(host="localhost", username=argv[1], password=argv[2],
+                         database=argv[3], port=3306) as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        table = cursor.fetchall()
+        for data in table:
+            print(data)
