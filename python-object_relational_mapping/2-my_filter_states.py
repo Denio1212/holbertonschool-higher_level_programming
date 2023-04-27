460f3abd2b9ebfd60a9781737dbee68b7fcd3d68
Denio <shehuden22@yahoo.com>
2023-04-27 14:54:48 +0200

    task 3

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ Returns all states matchiing the argument
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    inputs = argv[4]
+
+    with MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
+                         database=argv[3], port=3306) as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name Like '%{}%'".format(inputs))
+        table = cursor.fetchall()
+        for data in table:
+            print(data)
