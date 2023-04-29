aca416418537c2308cf6b72fdad4305d64f99351
Denio <shehuden22@yahoo.com>
2023-04-29 17:14:25 +0200

    task 7

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
-The model of a state
-"""
+""" The model of a state """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
 class State(Base):
-    '''
-    inherits from the base class
-    '''
+    """ inherits from the base class """
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
