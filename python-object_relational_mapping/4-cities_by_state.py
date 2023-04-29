e4a3d2403629a620603be4afcfa174a1c98a2f47
Denio <shehuden22@yahoo.com>
2023-04-29 15:11:20 +0200

    task 5

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,3 +16,6 @@ if __name__ == "__main__":
                        "JOIN states"
                        "ON cities.state_id = states.id"
                        "ORDER BY cities.id ASC")
+        content = cursor.fetchall()
+        for data in content:
+            print(data)
