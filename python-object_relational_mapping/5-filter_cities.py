02292dc2cca6f9fa43bd1d8930a01da31d08d782
Denio <shehuden22@yahoo.com>
2023-04-29 16:28:24 +0200

    task 6

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
         executee = "SELECT *" \
                    "FROM cities" \
                    "JOIN states" \
-                   "ON cities.state_id = states.id" \
-                   "WHERE states.name=%s ORDER BY cities.id ASC"
+                   " ON cities.state_id = states.id" \
+                   " WHERE states.name=%s ORDER BY cities.id ASC"
         cursor.execute(executee, ('%' + inputs + '%',))
         content = cursor.fetchall()
         outcome = []
