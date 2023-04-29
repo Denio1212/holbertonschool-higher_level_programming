c4077d2bbf9d68a6dee56deb49853134815ec315
Denio <shehuden22@yahoo.com>
2023-04-29 15:09:08 +0200

    task 5

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Listing the cities in the database through ORM
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    with MySQLdb.connect(host="localhost", port=3306, user=argv[1], password=argv[2],
+                         database=argv[3], charset="utf8") as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT cities.id, cities.name, states.name"
+                       "FROM cities"
+                       "JOIN states"
+                       "ON cities.state_id = states.id"
+                       "ORDER BY cities.id ASC")
