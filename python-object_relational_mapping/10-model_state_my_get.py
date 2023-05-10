25197616cfe13ba29c2314cdc8581d90dc35617e
Denio <shehuden22@yahoo.com>
2023-05-10 14:54:38 +0200

    task 11

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""10-model_state_my_get module
+The same as the previous but with changing arguments
+
+nothing too special in this one
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for data in session.query(State).filter(State.name.like('%{}%').format(argv[4])).\
+            order_by(State.id).all():
+        print(data.id)
+        exit()
+    print("Not found")
+    session.close()
