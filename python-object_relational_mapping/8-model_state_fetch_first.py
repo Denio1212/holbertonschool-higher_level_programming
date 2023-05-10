dd969eaa397c9ebeb69c5873a42ae6945a2cc743
Denio <shehuden22@yahoo.com>
2023-05-10 14:16:07 +0200

    task 9

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""8-model_state_fetch_first module
+a script that prints the first object from the database
+
+first -> returns the first object in a sql query
+under a given criteria
+query -> it basically allows accessing and tinkering
+the tables
+"""
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data = session.query(State).first()
+    if data:
+        print("{}: {}".format(data.id, data.name))
+    else:
+        print("Nothing")
