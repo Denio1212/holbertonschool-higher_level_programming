710e039fc70d7f0d7033af052b81c7ae36bf1c1d
Denio <shehuden22@yahoo.com>
2023-05-11 16:15:43 +0200

    task 15 and a change

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""13-model_state_delete_a module
-Deletes all objects containing the letter a
-in the database
-
+"""14-model_city_fetch_by_state module
+Prints all city objects in the database
+Using joins now
 """
-
 
 if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from sys import argv
     from model_state import Base, State
+    from model_city import City
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1],
                            argv[2], argv[3]), pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.query(State).filter_by(name="%a%").delete()
-    session.commit()
+    for city, state in session.query(City, State).join(State).order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
