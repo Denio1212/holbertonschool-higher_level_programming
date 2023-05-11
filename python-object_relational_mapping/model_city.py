84f493da57d860b4164b35f229bbfd09fe5bc94c
Denio <shehuden22@yahoo.com>
2023-05-11 16:16:16 +0200

    task 15 and a change

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""model_city module
+Contains the definition of a city
+
+Foreign key that connects to the states.id and everything
+else is similar to the state class
+"""
+from sqlalchemy.orm import relationship
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """City class
+    The base class for any city
+    """
+    state = relationship("State")
+    __tablename__ = "cities"
+    id = Column(Integer, unique=True, nullable=False,
+                primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey="states.id")
+
