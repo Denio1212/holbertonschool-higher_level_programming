b02a0e1875b75d2d6b83779ee156abd07059ae55
Denio <shehuden22@yahoo.com>
2023-05-11 16:24:51 +0200

    change

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
     from model_state import State, Base
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                           argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                           argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
