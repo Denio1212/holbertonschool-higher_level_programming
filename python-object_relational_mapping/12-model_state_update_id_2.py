c0199370ab12f83bc61641637c38b9ceed273c1c
Denio <shehuden22@yahoo.com>
2023-05-11 12:52:43 +0200

    task 13

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""12-model_state_update_id_2 module
+Changes the name of state whose id is 2
+***
+Class.(attribute) -> can be used to change all sorts
+of things
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    mehico = session.query(State).filter_by(id=2)
+    if mehico:
+        State.name = "New Mexico"
+        session.commit()
+    session.close()
