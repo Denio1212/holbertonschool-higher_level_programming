386d8f43789602667a7ab1b781041c2dd49c91a5
Denio <shehuden22@yahoo.com>
2023-05-11 16:22:13 +0200

    task 15 and a change

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     from sys import argv
     from model_state import Base, State
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1],
-                           argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.query(State).filter_by(name="%a%").delete()
+    session.query(State).filter(State.name.like("%a%")).delete()
     session.commit()
     session.close()
