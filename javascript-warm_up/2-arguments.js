5279f04a25aa48b2b583e425bfc5d7719ccad899
Denio <shehuden22@yahoo.com>
2023-06-22 23:37:04 +0200

    ARGV

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,8 +5,8 @@ const Countargs = process.argv.length - 2;
 /* The reason its -2 is beacuse it skips the script ending */
 if (Countargs == 0) {
 	console.log('No argument');
-}	else if (Countargs == 1) {
+} else if (Countargs == 1) {
 	console.log('Argument found');
-}	else {
+} else {
 	console.log('Arguments found')
 }
