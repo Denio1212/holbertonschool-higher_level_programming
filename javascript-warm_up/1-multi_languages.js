0292f5f665fb7b2cc7f7b0be935e3fb0eb705482
Denio <shehuden22@yahoo.com>
2023-06-22 23:21:34 +0200

    Multi task

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* A simple print function */
+const Line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+console.log(Line1);
+console.log(line2);
+console.log(line3);
