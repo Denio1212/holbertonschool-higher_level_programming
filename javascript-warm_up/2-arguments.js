9a387a4ae18651183e80a26b5a18f511e076c625
Denio <shehuden22@yahoo.com>
2023-06-22 23:30:13 +0200

    ARGV

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+	#!/usr/bin/node
+
+	/* The message is as large as your imagination */
+	const Countargs = process.argv.length - 2;
+    /* The reason its -2 is beacuse it skips the script ending */
+	if (Countargs == 0) {
+		console.log('No argument');
+	}
+	else if (Countargs == 1) {
+		console.log('Argument found');
+	}
+	else {
+		console.log('Arguments found')
+	}
