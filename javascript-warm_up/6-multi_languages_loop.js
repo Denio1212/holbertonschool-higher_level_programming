b025f5d6710aa499e74394e27068ffcdb52db2f0
Denio <shehuden22@yahoo.com>
2023-06-24 18:06:59 +0200

    oopsie

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@
 /* Same as 1 but with loop */
 const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let i = 0; array[i]; i++) {
+for (let i = 0; list[i]; i++) {
   console.log(list[i]);
 }
