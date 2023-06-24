3894c6b3c84761445155b6fdc5dbd69bc6a80d3d
Denio <shehuden22@yahoo.com>
2023-06-24 18:12:43 +0200

    SS

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 /* Prints the set times */
-if (!procces.argv[2]) {
+if (!process.argv[2]) {
   console.log('Missing number of occurences');
-} else if (procces.argv[2] > 0) {
-  for (let i = procces.argv[2]; i; i--) {
+} else if (process.argv[2] > 0) {
+  for (let i = process.argv[2]; i; i--) {
     console.log('C is fun');
   }
 }
