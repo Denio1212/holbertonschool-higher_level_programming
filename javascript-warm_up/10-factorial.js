60a0b97e8e0c1af2eff739848cb6d249b5133dc4
Denio <shehuden22@yahoo.com>
2023-06-24 18:28:14 +0200

    Facs

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/* makes the factorial happen */
+
+function factorial (n) {
+  if (!n) {
+    return 1;
+  } else if (n > 0) {
+    return n * factorial(n-1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
