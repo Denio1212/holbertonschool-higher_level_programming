db5cd0e8c712364f1f2fde6e1fcd4755c4b42474
Denio <shehuden22@yahoo.com>
2023-06-24 18:01:16 +0200

    fix

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 /* converting to int */
 if (!parseInt(process.argv[2])) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
-  console.log('My number: %d', parseInt(process.argv[2]))
+  console.log('My number: %d', parseInt(process.argv[2]));
 }
