0cd26f0fe8ba0f21261ae6bb29770c8fdc46b421
Denio <shehuden22@yahoo.com>
2023-06-24 18:34:31 +0200

    Len -1

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* Second only */
+if (process.argv.length == 2 || process.argv.length == 3) {
+  console.log('0');
+} else {
+  const list = process.argv.slice(2).sort((a, b) => a - b);
+  const len = array.length;
+
+  console.log(array[len - 1]);
+}
