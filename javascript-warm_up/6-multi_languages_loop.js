9cf165568553cca40fdf6659b182df182fee3d4b
Denio <shehuden22@yahoo.com>
2023-06-24 18:06:13 +0200

    Loop Pool

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* Same as 1 but with loop */
+const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; array[i]; i++) {
+  console.log(list[i]);
+}
