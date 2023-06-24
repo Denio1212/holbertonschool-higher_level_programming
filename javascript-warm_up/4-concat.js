fd1627790cbbf8ea900db059b2596ea8e3de4bc0
Denio <shehuden22@yahoo.com>
2023-06-24 17:52:18 +0200

    fix2

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -4,5 +4,5 @@
 if (!process.argv[2]) {
   console.log('undefined is undefined');
 } else {
-  console.log(process.argv[2].concat(' is ', process.argv[3]))
+  console.log(process.argv[2].concat(' is ', process.argv[3]));
 }
