c0542ecef9a63f73099f044bc5b64ddce21c8298
Denio <shehuden22@yahoo.com>
2023-06-25 16:39:05 +0200

    t1

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], function(err, data) {
+fs.readFile(process.argv[2], function (err, data) {
   if (err) console.log(err);
   else process.stdout.write(data);
 });
