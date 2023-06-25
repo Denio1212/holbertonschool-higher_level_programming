2e033e00df7362b17574afcb040f0d6f87e86316
Denio <shehuden22@yahoo.com>
2023-06-25 17:28:06 +0200

    t4

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,9 @@
 /**
  * Prints the name of the star wars
  * Title depending on the given int
- * const request = require('request');
+ */
+
+const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/${process.argv[2]}';
 
 request.get(url, (error, response, body) => {
@@ -14,11 +16,3 @@ request.get(url, (error, response, body) => {
     console.log(Data.title);
   }
 });
- */
-const request = require('request');
-
-request.get(`https://swapi.co/api/films/${process.argv[2]}/`,
-  function (error, response, body) {
-    if (error) console.log(error);
-    else console.log(JSON.parse(body).title);
-  });
